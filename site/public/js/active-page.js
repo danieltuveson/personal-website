eb78eaa5b4df4462ca1e34e2d9c017bf21b597ef
Daniel Tuveson <danieltuveson@gmail.com>
2019-01-03 13:04:27 -0800

    created a rough layout of the site

---
+++ site/public/js/active-page.js
@@ -0,0 +1,28 @@
+const url = document.URL;
+
+// returns true if url matches the regex, else false
+const isPage = (regex) => {
+  return url.search(regex) !== -1;
+}
+
+// returns current page's as a string
+const getCurrentPage = (url) => {
+  let currentPage;
+
+  if (isPage(/about/i)) {
+    currentPage = 'about';
+  } else if (isPage(/resume/i)) {
+    currentPage = 'resume';
+  } else if (isPage(/portfolio/i)) {
+    currentPage = 'portfolio';
+  } else if (isPage(/contact/i)) {
+    currentPage = 'contact';
+  } else {
+    currentPage = 'home';
+  }
+
+  return currentPage + 'Nav';
+}
+
+let elt = document.getElementById(getCurrentPage(url));
+elt.className += ' active';
